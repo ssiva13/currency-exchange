969154cc45c0f07dde5936db4f0237de0bd8d909
Simon Siva <simonsiva13@gmail.com>
2023-04-10 14:15:59 +0300

    create route

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Date 10/04/2023
+ * @author   Simon Siva <simonsiva13@gmail.com>
+ */
+
+
+use Ssiva\CurrencyExchange\Controllers\CurrencyConverterController;
+
+Route::get('currency-exchange', [CurrencyConverterController::class, 'exchange'])->name('currency-exchange');
+
