3f123bae563957d2282e97bce9841fb330955dc3
Simon Siva <simonsiva13@gmail.com>
2023-04-10 15:43:44 +0300

    create test cases

---
+++ tests/CurrencyExchangeTest.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Date 10/04/2023
+ * @author   Simon Siva <simonsiva13@gmail.com>
+ */
+
+namespace Ssiva\CurrencyExchange\Tests;
+
+use Orchestra\Testbench\TestCase;
+use Ssiva\CurrencyExchange\CurrencyExchangeServiceProvider;
+
+class CurrencyExchangeTest extends TestCase
+{
+    public function testConvert()
+    {
+        $response = $this->get('/api/v1/currency-exchange?amount=100&currency=USD');
+        $response->assertStatus(200);
+        $response->assertJson([
+            'converted_amount' => 490.49,
+            'currency' => 'RON',
+        ]);
+    }
+
+    public function testDefaultConfig(): void
+    {
+        $configValue = config('currency-exchange.default_currency');
+        $this->assertEquals('EUR', $configValue);
+    }
+
+    public function testCustomConfig(): void
+    {
+        config(['currency-exchange.default_currency' => 'USD']);
+        $configValue = config('currency-exchange.default_currency');
+        $this->assertEquals('USD', $configValue);
+    }
+
+    protected function getPackageProviders($app): array
+    {
+        return [
+            CurrencyExchangeServiceProvider::class,
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app): void
+    {
+        $app['config']->set('app.debug', true);
+    }
+
+}
