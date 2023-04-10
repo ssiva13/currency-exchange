57b71b80450f7d7e3cc4e62d88059ec1abd78b21
Simon Siva <simonsiva13@gmail.com>
2023-04-10 14:15:34 +0300

    create contoller to fetch currency rates

---
+++ src/Controllers/CurrencyConverterController.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Date 10/04/2023
+ * @author   Simon Siva <simonsiva13@gmail.com>
+ */
+
+namespace Ssiva\CurrencyExchange\Controllers;
+
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Validator;
+use OpenApi\Annotations as OA;
+use Ssiva\CurrencyExchange\Exchange\Exchange;
+
+class CurrencyConverterController extends Controller
+{
+    /**
+     * @OA\Get(
+     *     path="/api/currency-exchange",
+     *     summary="Convert currency",
+     *     tags={"Currency Converter"},
+     *     @OA\Parameter(
+     *         name="amount",
+     *         in="query",
+     *         description="The amount to convert",
+     *         required=true,
+     *         @OA\Schema(type="number")
+     *     ),
+     *     @OA\Parameter(
+     *         name="currency",
+     *         in="query",
+     *         description="The currency to convert to",
+     *         required=false,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Conversion result",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="amount", type="number", example="1.23"),
+     *             @OA\Property(property="currency", type="string", example="USD")
+     *         )
+     *     )
+     * )
+     * @throws GuzzleException
+     */
+    public function exchange(Request $request): JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'amount' => 'required|numeric',
+            'currency' => 'string|max:4|min:2'
+        ]);
+        if($validator->fails()){
+            return response()->json($validator->errors(), 400);
+        }
+        $amount = $request->get('amount', 1);
+        $toCurrency = $request->get('currency', 'EUR');
+
+        // Fetch exchange rate from ECB daily reference
+        $exchangeRate = (new Exchange())->getExchangeRate($toCurrency);
+
+        if (!$exchangeRate) {
+            return response()->json([
+                'error' => 'Invalid currency'
+            ], 400);
+        }
+
+        $total = $amount * $exchangeRate;
+
+        return response()->json([
+            'amount' => $total,
+            'currency' => $toCurrency
+        ]);
+    }
+}
